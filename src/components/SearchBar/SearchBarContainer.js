53aa3ac1a97a4568791fde2bd67e8e97315263e2
Jorge Jimenez <calenocam24@gmail.com>
2020-06-20 15:26:26 -0400

    added moree styles to SearchBarContainer

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,7 +1,7 @@
 // You do not need to change any code in this file
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faSearch } from '@fortawesome/free-brands-svg-icons'
 import { faHeart, faCompass, faUser } from '@fortawesome/free-regular-svg-icons'
 import "./SearchBar.css";
 
@@ -11,12 +11,15 @@ const SearchBar = () => {
       <div className="social" id="one">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
+      <div className="line">
+      <hr size="25"></hr>
+      </div>
       <div className="instagram-letter">
         <h3>𝓘𝓷𝓼𝓽𝓪𝓰𝓻𝓪𝓶</h3>
       </div>
       <form className="search-form">
         <input
-          type="text" placeholder=" 🔍 Search"
+          type="text" placeholder="🔍 Search"
         />
       </form>
       <div className="social-wrapper">
