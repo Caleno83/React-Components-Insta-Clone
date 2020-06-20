89f979baa7d5a385fceed34632feefb043779070
Jorge Jimenez <calenocam24@gmail.com>
2020-06-20 11:31:37 -0400

    setup, and added states for comments

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+    console.log('props info from CommentSection', props);
+
+    const [comments, setComments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(com => (
+        <Comment key={com.id} comment={com} />
+      ))}
       <CommentInput />
     </div>
   );
